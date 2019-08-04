21dcf3b6eae502dd08ddfb1a12578d644155c89f
Muhammad-Bilal-200193 <53682198+Muhammad-Bilal-200193@users.noreply.github.com>
2019-08-03 22:28:22 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,9 +2,7 @@ ans = 0
 prompt = ""
 while prompt.lower() != "q":
     prompt = input("enter num:")
-    #if prompt == "":
-     #   print(ans)
-
+   
     prompt = prompt.strip()
     if prompt[0] == "+":
             ans = ans + int(prompt[1:])
